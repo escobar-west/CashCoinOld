0816ac025a9e75fea64bf921539640735464cfe0
Victor Suriel <victor.v.suriel@gmail.com>
2018-05-12 12:08:40 -0400

    none of your business

--- accounts/CoinbaseWalletClient.py
+++ accounts/CoinbaseWalletClient.py
@@ -1,6 +1,6 @@
 from coinbase.wallet.client import Client
 
 client = Client(
-        'gtFVJWxtnLnrOgKw',
-        'd1DQcaGHGpe8NzMEVK0Eedocd36tFh5j',
+        'XXXXXX',
+        'XXXXXXXXXXXXXXXXX',
         api_version='2017-10-17',)
